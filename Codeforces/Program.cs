eb25ed952b32241a7bee982419192f558f816197
Ahmed Saleh <>
2019-09-14 18:35:55 +0200

    CF-Watermelon

--- Codeforces/Program.cs
+++ Codeforces/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            WordCapitalization.Run();
+            Watermelon.Run();
         }
     }
 }
---
+++ Codeforces/Watermelon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Codeforces
+{
+    public class Watermelon
+    {
+        public static void Run()
+        {
+            const int MIN_EVEN_DIVISION = 2;
+
+            int weight = int.Parse(Console.ReadLine());
+            int division = weight - MIN_EVEN_DIVISION;
+
+            if (division > 0 && (division % 2) == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else 
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
